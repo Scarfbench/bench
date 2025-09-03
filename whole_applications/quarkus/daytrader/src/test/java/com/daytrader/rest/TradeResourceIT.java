617d21c4e0b2681c3cd52028d237c88de387f0b0
Srikanth G Tamilselvam <srikuklose@gmail.com>
2025-09-03 12:30:31 +0530

    daytrader->quarkus

---
+++ whole_applications/quarkus/daytrader/src/test/java/com/daytrader/rest/TradeResourceIT.java
@@ -0,0 +1,8 @@
+package com.daytrader.rest;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class TradeResourceIT extends TradeResourceTest {
+    // Execute the same tests but in packaged mode.
+}
