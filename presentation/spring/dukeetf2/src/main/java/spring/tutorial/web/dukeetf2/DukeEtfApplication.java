f5fddd7effd5c3ddcdcad1f578297bbce3a92ddd
Bridget McGinn <bridget.mcginn@ibm.com>
2025-09-11 16:43:58 -0400

    add dukeetf2 to spring updates

---
+++ presentation/spring/dukeetf2/src/main/java/spring/tutorial/web/dukeetf2/DukeEtfApplication.java
@@ -0,0 +1,21 @@
+package spring.tutorial.web.dukeetf2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
+
+@SpringBootApplication
+@EnableScheduling
+public class DukeEtfApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DukeEtfApplication.class, args);
+    }
+
+    @Bean
+    public ServerEndpointExporter serverEndpointExporter() {
+        return new ServerEndpointExporter();
+    }
+}
